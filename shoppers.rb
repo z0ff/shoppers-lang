8241ed459e44a2785835d8a0117d51cfe90a9d35
z0ff <ex1999@msn.com>
2019-11-23 02:54:33 +0900

    first commit

---
+++ shoppers.rb
@@ -0,0 +1,15 @@
+Encoding.default_external = "UTF-8"
+require 'r-fxxk'
+
+class Shoppers < Brainfuck
+  nxt "´"
+  inc "ン"
+  prv "ョ"
+  dec "゛"
+  opn "ノ"
+  cls "ハ"
+  put "＼"
+  get "ー"
+end
+
+puts Shoppers.new.fuck(ARGF.read)
